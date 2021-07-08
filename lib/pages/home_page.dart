66dcb470cc2ebc78f9e9bcd8626a86e09e28b1f8
PerairaCC <carloscoto0103@gmail.com>
2021-07-07 20:42:51 -0600

    Clase 1 Seccion 23

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class HomePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Pagar'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text('HomePage'),
+     ),
+   );
+  }
+}
